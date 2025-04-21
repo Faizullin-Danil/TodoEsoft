90050407a55a2c61d60179f035cb8620afd0b885
Danil Faizullin <toxen2005@mail.ru>
2025-04-21 11:33:39 +0500

    last

--- Frontend/src/http/index.ts
+++ Frontend/src/http/index.ts
@@ -1,8 +1,8 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
-// const API_URL = 'http://localhost:3000/';
+const API_URL = 'http://localhost:3000/';
 
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
 
 
 const $api = axios.create({
